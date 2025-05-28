685131b94bbeaca183f9610c64500d38f696cbeb
PatrickLeonardo <patricklf7@proton.me>
2025-05-28 14:39:43 -0300

    main: update controllers and models

--- src/main/java/server/controllers/ClientController.java
+++ src/main/java/server/controllers/ClientController.java
@@ -33,19 +33,6 @@ public class ClientController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping(path = "findUserByCPFAndPassword")
-    public ResponseEntity<Null> findUserByCPFAndPassword(@Valid @RequestParam final String CPF, @Valid @RequestParam final String password) {
-        
-        if(clientRepository.findByCPF(CPF) != null) {
-            
-            if(userRepository.findByCpfAndSenha(CPF, password) != null) {  
-                return new ResponseEntity<>(HttpStatus.FOUND); // Status Code 302
-            } else return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Status Code 404
-            
-        } else return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Status Code 404
-        
-    }
-
     @GetMapping(path = "findClientByCPF")
     public ResponseEntity<Client> findClientIdByCPF(@Valid @RequestParam final String CPF) {
 
--- src/main/java/server/controllers/UserController.java
+++ src/main/java/server/controllers/UserController.java
@@ -1,44 +1,73 @@
 package server.controllers;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import jakarta.validation.Valid;
-import server.repository.UserRepository;
+import jakarta.validation.constraints.Null;
 import server.model.User;
+import server.repository.ClientRepository;
+import server.repository.UserRepository;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    private UserRepository useRepository;
+    private ClientRepository clientRepository; 
 
-    @GetMapping(path = "findByCPF")
-    public ResponseEntity<User> findByCpf(@Valid @RequestParam final String CPF) {
+    @Autowired
+    private UserRepository userRepository;
 
-        return ResponseEntity.ok(useRepository.findByCpf(CPF));
+    @GetMapping(path = "find/{cpf}/{password}")
+    public ResponseEntity<Void> find(@Valid @PathVariable final String cpf, @Valid @PathVariable final String password) { 
+        
+        if(clientRepository.findByCPF(cpf) != null)
+            return ResponseEntity.status((userRepository.findByCpfAndSenha(cpf, password) != null) ? 302 : 404).build();
+        
+        else return ResponseEntity.status(404).build(); // Status Code 404    
+        
+    }
+
+    @GetMapping(path = "findName/{cpf}")
+    public ResponseEntity<String> findName(@Valid @PathVariable final String cpf) {
+
+        User user = userRepository.findByCpf(cpf);
+        if(user != null)
+            return ResponseEntity.status(302).body(user.getNome());
+
+        else return ResponseEntity.status(404).build();
+
+    }
+
+    @GetMapping(path = "getUser/{cpf}/{password}")
+    public ResponseEntity<User> getUser(@Valid @PathVariable final String cpf, @Valid @PathVariable final String password) {
+        
+        User findedUser = userRepository.findByCpfAndSenha(cpf, password);
+        
+        return ResponseEntity.status((findedUser != null) ? 302 : 404).body(findedUser);
 
     }
 
     @PutMapping(path = "update/{cpf}")
     public ResponseEntity<User> updateUser(@Valid @RequestBody User user, @Valid @PathVariable String cpf) {
 
-        User existentUser = useRepository.findByCpf(cpf);
+        User existentUser = userRepository.findByCpf(cpf);
         
         existentUser.setNome(user.getNome());
         existentUser.setSenha(user.getSenha());
         existentUser.setTelefone(user.getTelefone());
         existentUser.setEndereco(user.getEndereco());
 
-        useRepository.save(existentUser);
+        userRepository.save(existentUser);
 
         return ResponseEntity.ok(existentUser);
 
